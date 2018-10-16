fb12fb7c286e90b5c3876c4db95e401a47b71c61
Deng Jun <proudeng@gmail.com>
2018-10-16 17:33:05 +0800

    add mycrypt.py file and refactor AutoTimeReport.py.
    can not run it now.

--- AutoTimeReport.py
+++ AutoTimeReport.py
@@ -1,92 +1,104 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from mycrypt import prpcrypt
+import json
 
-browser = webdriver.Ie()
-browser.get("https://ep.ss.sw.ericsson.se/irj/portal")
-browser.implicitly_wait(30)
-
-try:
-    timeReportPage = browser.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Time Sheet Status'])[1]/following::span[2]")
-except NoSuchElementException :
-    usr = browser.find_element_by_id("logonuidfield")
-    usr.clear()
-    usr.send_keys("edenjun")
-    passwd = browser.find_element_by_id("logonpassfield")
-    passwd.clear()
-    passwd.send_keys("Storyer1810")
-    browser.find_element_by_name("uidPasswordLogon").click()
-
-    timeReportPage = browser.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Time Sheet Status'])[1]/following::span[2]")
-
-#Enter the time reporting page
-timeReportPage.click()
-
-browser.switch_to_frame("contentAreaFrame")
-browser.switch_to_frame("isolatedWorkArea")
-
-#find the report plan
-date1_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE1HeaderCaption015")
-date2_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE2HeaderCaption016")
-date3_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE3HeaderCaption017")
-date4_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE4HeaderCaption018")
-date5_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE5HeaderCaption019")
-date6_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE6HeaderCaption020")
-date7_plan = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE7HeaderCaption021")
-
-#update the report with assumption that we fill in the first row
-date1_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE1_InputField.0")
-date1_report.click()
-date1_report.clear()
-date1_report.send_keys(date1_plan.text)
-
-date2_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE2_InputField.0")
-date2_report.click()
-date2_report.clear()
-date2_report.send_keys(date2_plan.text)
-
-date3_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE3_InputField.0")
-date3_report.click()
-date3_report.clear()
-date3_report.send_keys(date3_plan.text)
-
-date4_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE4_InputField.0")
-date4_report.click()
-date4_report.clear()
-date4_report.send_keys(date4_plan.text)
-
-date5_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE5_InputField.0")
-date5_report.click()
-date5_report.clear()
-date5_report.send_keys(date5_plan.text)
-
-date6_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE6_InputField.0")
-date6_report.click()
-date6_report.clear()
-date6_report.send_keys(date6_plan.text)
-
-date7_report = browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE7_InputField.0")
-date7_report.click()
-date7_report.clear()
-date7_report.send_keys(date7_plan.text)
-
-#save reporing 
-browser.find_element_by_id("aaaaCEAO.LinkBarView.RootUIElementContainer").click()
-browser.find_element_by_id("aaaaKEBH.VcCatRecordEntryView.ButtonNext").click()
-
-browser.find_element_by_id("aaaaLBOD.VcGenericButtonView.Save_com_sap_xss_hr_cat_record_vac_review_VcCatRecordReview").click()
-
-class webhandler():
-    def __init__(self, browser):
-        if browser == "ie":
-            webbrowser = webdriver.Ie()
+class browserhandler():
+    def __init__(self, webbrowser):
+        if webbrowser == "ie":
+            self.browser = webdriver.Ie()
         elif browser == "firefox":
-            webbrowser = webdriver.Firefox()
-        webbrowser.implicitly_wait(10)
+            self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
     
-    def reportTime(self,eid):
-        fdsa
+    def reportTime(self,eid, passwd):
+        self.browser.get("https://ep.ss.sw.ericsson.se/irj/portal")
+        
+        try:
+            timeReportPage = self.browser.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Time Sheet Status'])[1]/following::span[2]")
+        except NoSuchElementException :
+            usr = self.browser.find_element_by_id("logonuidfield")
+            usr.clear()
+            usr.send_keys(eid)
+            passwd = self.browser.find_element_by_id("logonpassfield")
+            passwd.clear()
+            passwd.send_keys(passwd)
+            self.browser.find_element_by_name("uidPasswordLogon").click()
+        
+            timeReportPage = self.browser.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Time Sheet Status'])[1]/following::span[2]")
+        
+        #Enter the time reporting page
+        timeReportPage.click()
+        
+        self.browser.switch_to_frame("contentAreaFrame")
+        self.browser.switch_to_frame("isolatedWorkArea")
+        
+        #find the report plan
+        date1_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE1HeaderCaption015")
+        date2_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE2HeaderCaption016")
+        date3_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE3HeaderCaption017")
+        date4_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE4HeaderCaption018")
+        date5_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE5HeaderCaption019")
+        date6_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE6HeaderCaption020")
+        date7_plan = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE7HeaderCaption021")
+        
+        #update the report with assumption that we fill in the first row
+        date1_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE1_InputField.0")
+        date1_report.click()
+        date1_report.clear()
+        date1_report.send_keys(date1_plan.text)
+        
+        date2_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE2_InputField.0")
+        date2_report.click()
+        date2_report.clear()
+        date2_report.send_keys(date2_plan.text)
+        
+        date3_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE3_InputField.0")
+        date3_report.click()
+        date3_report.clear()
+        date3_report.send_keys(date3_plan.text)
+        
+        date4_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE4_InputField.0")
+        date4_report.click()
+        date4_report.clear()
+        date4_report.send_keys(date4_plan.text)
+        
+        date5_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE5_InputField.0")
+        date5_report.click()
+        date5_report.clear()
+        date5_report.send_keys(date5_plan.text)
+        
+        date6_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE6_InputField.0")
+        date6_report.click()
+        date6_report.clear()
+        date6_report.send_keys(date6_plan.text)
+        
+        date7_report = self.browser.find_element_by_id("aaaaKEBH.VcCatTableWeek.WORKDATE7_InputField.0")
+        date7_report.click()
+        date7_report.clear()
+        date7_report.send_keys(date7_plan.text)
+        
+        #save reporting 
+        self.browser.find_element_by_id("aaaaCEAO.LinkBarView.RootUIElementContainer").click()
+        self.browser.find_element_by_id("aaaaKEBH.VcCatRecordEntryView.ButtonNext").click()
+        
+        self.browser.find_element_by_id("aaaaLBOD.VcGenericButtonView.Save_com_sap_xss_hr_cat_record_vac_review_VcCatRecordReview").click()
 
-
+    
 
 if __name__ == '__main__':
-    print("temp print")
+    browser = browserhandler("ie")
+    pc = prpcrypt("proudengxiaoshee")
+    try:
+        fb = open("user.json", 'r')
+    except FileNotFoundError:
+        print("user configuration file not existing!")
+    user = json.load(fb)
+    for eid in user.keys():
+        passwd = user[eid]["passwd"]
+        passwd = pc.decrypt(passwd)
+        print(passwd)
+#        browser.reportTime(eid, passwd)
+         
+        
+    
---
+++ mycrypt.py
@@ -0,0 +1,28 @@
+'''
+Created on 2018/10/12
+
+@author: edenjun
+'''
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+
+class prpcrypt():
+    def __init__(self, key):
+        self.key = key
+        self.mode = AES.MODE_CBC
+
+    def encrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.key)
+        length = 16
+        count = len(text)
+        add = length - (count % length)
+        text = text + ('\0' * add)
+        self.ciphertext = cryptor.encrypt(text)
+        return str(b2a_hex(self.ciphertext), encoding='utf-8')
+#        return self.ciphertext
+
+    def decrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, self.key)
+        plain_text = cryptor.decrypt(a2b_hex(text))
+#        plain_text = cryptor.decrypt(text)
+        return plain_text.rstrip('\0')
